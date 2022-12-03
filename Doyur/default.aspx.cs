411bac77c36c1b9d84183f8cdae66b492c2a715b
emrecoskun705 <emrecoskun705@gmail.com>
2022-12-03 23:18:13 +0300

    add restaurant list to default page

--- Doyur/default.aspx.cs
+++ Doyur/default.aspx.cs
@@ -9,9 +9,21 @@ namespace Doyur
 {
     public partial class _default : System.Web.UI.Page
     {
+
+        db.doyurEntities db = new db.doyurEntities();
+
+        public List<db.sp_GetRestaurants_Result> RestaurantList { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
+                var getRestaurants = (from p in db.sp_GetRestaurants(10) select p).ToList();
 
+                if(getRestaurants != null && getRestaurants.Count > 0 ) {
+                    RestaurantList = getRestaurants;
+                }
+            }
         }
     }
 }
